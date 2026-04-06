e3d8afbcd35d394542ab5554d2cd34d75524c74f
Anton <osipozzz@vk.com>
2026-04-06 14:12:54 +0300

    дело сделано

---
+++ filecreate.py
@@ -0,0 +1,18 @@
+def createTXT():
+    with open("SS.txt", "w+") as SSS:
+        try:
+            print("enter text for file")
+            st = input("ввод:")
+            SSS.write(st)
+        finally:
+            print("ваш текст сохранен:)")
+            SSS.close()
+
+def main():
+    print("привет, хочешь ли создать текстовый файл?")
+    choice = input("y/n")
+    
+    if choice == "y":
+        createTXT()
+    else:
+        return
